bf052157b866f8e159436243c2d8cadd8bda1e3a
mroger25 <madsonmath@hotmail.com>
2021-09-15 03:26:30 -0300

    final

--- src/main.js
+++ src/main.js
@@ -3,19 +3,31 @@ function calcCurrency() {
   // Elementos
   const dolarEmReal = document.getElementById("dolarEmReal");
   const realEmDolar = document.getElementById("realEmDolar");
+  const dolarEmBit = document.getElementById("dolarEmBit");
+  const bitEmDolar = document.getElementById("bitEmDolar");
   const moeda = document.getElementById("moeda");
   const resultText = document.getElementById("result");
 
-  const cot = 5.23; // cotação do dólar no dia 13-09-2021
+  // cotação no dia 13-09-2021
+  const cotDolarReal = 5.23;
+  const cotDolarBit = 46961.5;
 
+  let resultValue, text;
   if (moeda.value !== "") {
-    let resultValue, text;
     if (dolarEmReal.checked) {
-      resultValue = Number((moeda.value * cot).toFixed(2));
+      resultValue = Number((moeda.value * cotDolarReal).toFixed(2));
       text = "O resultado em real é R$ " + resultValue;
       resultText.innerHTML = text;
     } else if (realEmDolar.checked) {
-      resultValue = Number((moeda.value / cot).toFixed(2));
+      resultValue = Number((moeda.value / cotDolarReal).toFixed(2));
+      text = "O resultado em dólar é US$ " + resultValue;
+      resultText.innerHTML = text;
+    } else if (dolarEmBit.checked) {
+      resultValue = Number((moeda.value / cotDolarBit).toFixed(6));
+      text = "O resultado em bitcoin é ₿ " + resultValue;
+      resultText.innerHTML = text;
+    } else if (bitEmDolar.checked) {
+      resultValue = Number((moeda.value * cotDolarBit).toFixed(2));
       text = "O resultado em dólar é US$ " + resultValue;
       resultText.innerHTML = text;
     } else {
