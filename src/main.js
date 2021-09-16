77f36bf7f42b20720889e0929f13c751b4e51ee3
mroger25 <madsonmath@hotmail.com>
2021-09-15 22:56:45 -0300

    conversor de moedas

--- src/main.js
+++ src/main.js
@@ -1,49 +1,40 @@
 // Conversor de moeda real para dólar
+
+// Elementos
+const dolarEmReal = document.getElementById("dolarEmReal");
+const bitEmReal = document.getElementById("bitEmReal");
+const moeda = document.getElementById("moeda");
+const resultText = document.getElementById("result");
+
+// Cotações
+const cotDolar = 5.23;
+const cotBit = 551654;
+
 function calcCurrency() {
-  // Elementos
-  const dolarEmReal = document.getElementById("dolarEmReal");
-  const realEmDolar = document.getElementById("realEmDolar");
-  const dolarEmBit = document.getElementById("dolarEmBit");
-  const bitEmDolar = document.getElementById("bitEmDolar");
-  const moeda = document.getElementById("moeda");
-  const resultText = document.getElementById("result");
-
-  // cotação no dia 13-09-2021
-  const cotDolarReal = 5.23;
-  const cotDolarBit = 46961.5;
-
-  let resultValue, text;
+  let text;
   if (moeda.value !== "") {
     if (dolarEmReal.checked) {
-      resultValue = Number((moeda.value * cotDolarReal).toFixed(2));
-      text = "O resultado em real é R$ " + resultValue;
-      resultText.innerHTML = text;
-    } else if (realEmDolar.checked) {
-      resultValue = Number((moeda.value / cotDolarReal).toFixed(2));
-      text = "O resultado em dólar é US$ " + resultValue;
-      resultText.innerHTML = text;
-    } else if (dolarEmBit.checked) {
-      resultValue = Number((moeda.value / cotDolarBit).toFixed(6));
-      text = "O resultado em bitcoin é ₿ " + resultValue;
-      resultText.innerHTML = text;
-    } else if (bitEmDolar.checked) {
-      resultValue = Number((moeda.value * cotDolarBit).toFixed(2));
-      text = "O resultado em dólar é US$ " + resultValue;
-      resultText.innerHTML = text;
+      text = conversorDeMoedaParaReal(moeda.value, cotDolar);
+    } else if (bitEmReal.checked) {
+      text = conversorDeMoedaParaReal(moeda.value, cotBit);
     } else {
       text = "Selecione qual conversão deseja fazer";
-      resultText.innerHTML = text;
     }
   } else {
-    text = "Digite o valor a ser convertido";
-    resultText.innerHTML = text;
+    text = "Digite o valor em real a ser convertido";
   }
+  resultText.innerHTML = text;
+}
 
-  document.getElementById("clear").addEventListener("click", function () {
-    resultText.innerHTML = "";
-  });
+function conversorDeMoedaParaReal(valorMoeda, cotacao) {
+  let resultValue = Number((valorMoeda * cotacao).toFixed(2));
+  return "O resultado em real é R$ " + resultValue;
 }
 
 document
   .getElementById("currencyButton")
   .addEventListener("click", calcCurrency);
+
+document.getElementById("clear").addEventListener("click", function () {
+  resultText.innerHTML = "";
+});
